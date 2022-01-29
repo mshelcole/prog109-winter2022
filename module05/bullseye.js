ad63258bdb3ea1a5b1afe02a0c427f8037fceda3
mshelcole <97192890+mshelcole@users.noreply.github.com>
2022-01-29 14:22:27 -0800

    Update bullseye.js

--- module05/bullseye.js
+++ module05/bullseye.js
@@ -1,30 +1,35 @@
-//var table = 3;             // Unit of table
-//var table = parseInt(prompt('Enter an integer: '));
-//operator = GetValueFromUser("operator");
+var table = 3;             // Unit of table
+
+operator = GetValueFromUser("operator");
 table = GetValueFromUser("table")
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
-el.innerHTML = GetTableContent(table);
-
-
-//function GetValueFromUser(valueType) {
-//var table = parseInt(prompt('Enter an integer: '));
-//}
+el.innerHTML = GetTableContent(operator, table);
 
 function GetValueFromUser(valueType) {
- // var table = parseInt(prompt('Enter an integer: '));
-
-   return prompt(valueType);
+  var greetingMessage = 'Hello. How are you?';
+  if (valueType == "operator") {
+    greetingMessage += "Enter addition or multiplication";
+  }
+  else
+  {
+    greetingMessage += "Enter table";
+  }
+  return prompt(greetingMessage);
 }
 
-
-function GetTableContent(table) {
+function GetTableContent(operator, table) {
   var i = 1;                 // Set counter to 1
-  var msg = '<h2>Multiplication Table</h2>';   // Message  
-
-
-  {// Do multiplication
+  var msg = '';              // Message  
+  if (operator === 'addition') {
+    // Do addition
+    while (i < 11) {
+      msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+      i++;
+    }
+  } else {
+    // Do multiplication
     while (i < 11) {
       msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
       i++;
