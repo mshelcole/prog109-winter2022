f1c0d9e91fbbed51b1d5a49b77e489c44a877b48
mshelcole <97192890+mshelcole@users.noreply.github.com>
2022-02-16 17:43:27 -0800

    Update validator.js

--- artifact04/validator.js
+++ artifact04/validator.js
@@ -1,109 +1,47 @@
-// Defining a function to display error message
-function printError(elemId, hintMsg) {
-    document.getElementById(elemId).innerHTML = hintMsg;
-}
-
 // Defining a function to validate form 
 function validateForm() {
-    // Retrieving the values of form elements 
-    var name = document.contactForm.name.value;
-    var email = document.contactForm.email.value;
-    var mobile = document.contactForm.mobile.value;
-    var country = document.contactForm.country.value;
-    var gender = document.contactForm.gender.value;
-    var hobbies = [];
-    var checkboxes = document.getElementsByName("hobbies[]");
-    for(var i=0; i < checkboxes.length; i++) {
-        if(checkboxes[i].checked) {
-            // Populate hobbies array with selected values
-            hobbies.push(checkboxes[i].value);
-        }
-    }
-    
-	// Defining error variables with a default value
-    var nameErr = emailErr = mobileErr = countryErr = genderErr = true;
-    
-    // Validate name
-    if(name == "") {
-        printError("nameErr", "Please enter your name");
-    } else {
-        var regex = /^[a-zA-Z\s]+$/;                
-        if(regex.test(name) === false) {
-            printError("nameErr", "Please enter a valid name");
-        } else {
-            printError("nameErr", "");
-            nameErr = false;
-        }
-    }
-    
-    // Validate email address
-    if(email == "") {
-        printError("emailErr", "Please enter your email address");
-    } else {
-        // Regular expression for basic email validation
-        var regex = /^\S+@\S+\.\S+$/;
-        if(regex.test(email) === false) {
-            printError("emailErr", "Please enter a valid email address");
-        } else{
-            printError("emailErr", "");
-            emailErr = false;
-        }
-    }
-    
-    // Validate mobile number
-    if(mobile == "") {
-        printError("mobileErr", "Please enter your mobile number");
-    } else {
-        var regex = /^[1-9]\d{9}$/;
-        if(regex.test(mobile) === false) {
-            printError("mobileErr", "Please enter a valid 10 digit mobile number");
-        } else{
-            printError("mobileErr", "");
-            mobileErr = false;
-        }
-    }
-    
-    // Validate country
-    if(country == "Select") {
-        printError("countryErr", "Please select your country");
-    } else {
-        printError("countryErr", "");
-        countryErr = false;
-    }
-    
-    // Validate gender
-    if(gender == "") {
-        printError("genderErr", "Please select your gender");
-    } else {
-        printError("genderErr", "");
-        genderErr = false;
-    }
-    
-    // Prevent the form from being submitted if there are any errors
-    if((nameErr || emailErr || mobileErr || countryErr || genderErr) == true) {
-       return false;
-    } else {
-        // Creating a string from input data for preview
-        var dataPreview = "You've entered the following details: \n" +
-                          "Full Name: " + name + "\n" +
-                          "Email Address: " + email + "\n" +
-                          "Mobile Number: " + mobile + "\n" +
-                          "Country: " + country + "\n" +
-                          "Gender: " + gender + "\n";
-        if(hobbies.length) {
-            dataPreview += "Hobbies: " + hobbies.join(", ");
-        }
-        // Display input data in a dialog box before submitting the form
-        alert(dataPreview);
-    }
-function isValid() {
-    if (firstName() //&&
-       // lastName()
-    )
-    return true;
+    var validFirstname = false;
+    var validLastname = false;
+    var validUsername = false;
+    var validPassword = false;
+    var errorMessages = "";
+
+
+    // Validate Firstname
+    if (myContact.firstname.value == null ||
+        myContact.firstname.value === "" ||
+        myContact.firstname.value > 20)
+        errorMessages += "<p> The firstname must be less than 20 characters</p>";
     else
-        document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
-        event.preventDefault();
-        return false;
+        validateUsername = true;
+
+    // Validate Lastname
+    if (myContact.lastname.value == null ||
+        myContact.lastname.value === "" ||
+        myContact.lastname.value > 20)
+        errorMessages += "<p> The lastname must be less than 20 characters</p>";
+    else
+        validateUsername = true;
+
+
+    // Validate Username Required 12
+    if (myContact.username.value.length > 12 ||
+        myContact.username.value === null ||
+        myContact.username.value === "")
+        errorMessages += "<p> The username must be less than 12 characters and is required</p>";
+    else
+        validateUsername = true;
+
+    // Validate Password 
+    if (myContact.password.value.length > 7 ||
+        myContact.password.value === null ||
+        myContact.password.value === "")
+        errorMessages += "<p> The password must be less than 7 characters and is required</p>";
+    else
+        validateUsername = true;
+
+    document.getElementById("errorMessages").innerHTML = errorMessages
+    return (validFirstname && validLastname && validateUsername && validatePassword);
+
 }
 
