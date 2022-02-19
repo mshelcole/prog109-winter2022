d3fdc46a8453a2f2d6dea67df74b021f38b2a1bb
mshelcole <97192890+mshelcole@users.noreply.github.com>
2022-02-18 21:56:27 -0800

    Update validator.js

--- artifact04/validator.js
+++ artifact04/validator.js
@@ -11,10 +11,10 @@ function validate(){
   var state = document.getElementById("state").value;
   var country = document.getElementById("country").value;
   var zipcode = document.getElementById("zipcode").value;
-  
   var errorMessage = document.getElementById("errorMessage");
   var letters = /^[A-Za-z]+$/;
   var numbers = /^[0-9]+$/;
+  
   errorMessage.style.padding = "10px";
   
   var text;
@@ -23,7 +23,7 @@ function validate(){
     firstname === "" ||
     firstname.length > 20 ||
     !firstname.match(letters)){
-    text = "firstname must be less than 20 characters and is required.  Only letters accepted";
+    text = "First Name is required. Must be less than 20 characters and letters only.";
     errorMessage.innerHTML = text;
     return false;
   }
@@ -32,13 +32,13 @@ function validate(){
     lastname === "" ||
     lastname.length > 50 ||
     !lastname.match(letters)){
-    text = "lastname must be less than 50 characters and is required.  Only letters accepted";
+    text = "Last Name is required. Must be less than 50 characters and letters only.";
     errorMessage.innerHTML = text;
     return false;
   }
   
    if(email.indexOf("@") == -1 || email.length < 6){
-    text = "Please Enter valid Email";
+    text = "Please enter a valid email.";
     errorMessage.innerHTML = text;
     return false;
   }
@@ -47,7 +47,7 @@ function validate(){
     phone === "" ||
     phone.length > 15 ||
     !phone.match(numbers)){
-    text = "The phone number must be less than 15 character";
+    text = "Phone is required.  Must be less than 15 characters and numbers only.";
     errorMessage.innerHTML = text;
     return false;
   }
@@ -55,7 +55,7 @@ function validate(){
     if(username === null ||
     username === "" ||
     username.length > 12 ){
-    text = "The username must be less than 12 characters and is required";
+    text = "Username is required.  Must be less than 12 characters.";
     errorMessage.innerHTML = text;
     return false;
   }
@@ -63,7 +63,7 @@ function validate(){
     if(password === null ||
     password === "" ||
     password.length > 7 ){
-    text = "The password must be less than 7 characters and is required";
+    text = "Password is required.  Must be less than 7 characters.";;
     errorMessage.innerHTML = text;
     return false;
   }
@@ -71,32 +71,26 @@ function validate(){
   
      if(address === null ||
     address === "" ){
-    text = "Address is required";
+    text = "Address is required.";
     errorMessage.innerHTML = text;
     return false;
   }
   
        if(city === null ||
     city === "" ){
-    text = "City is required";
+    text = "City is required.";
     errorMessage.innerHTML = text;
     return false;
   }
   
       if(state === "000"  ){
-    text = "State is required";
+    text = "State is required.";
     errorMessage.innerHTML = text;
     return false;
   }
   
         if(country === "000"  ){
-    text = "Country is required";
-    errorMessage.innerHTML = text;
-    return false;
-  }
-  
-          if(country === "000"  ){
-    text = "Country is required";
+    text = "Country is required.";
     errorMessage.innerHTML = text;
     return false;
   }
@@ -104,14 +98,13 @@ function validate(){
      
     if(country === 1  ||
     zipcode.length != 5 ){
-    text = "Zipcode is required";
+    text = "Zipcode is required.";
     errorMessage.innerHTML = text;
     return false;
               
   }
   
   
-  
-  //alert("Form Submitted Successfully!");
+
   return true;
 }
