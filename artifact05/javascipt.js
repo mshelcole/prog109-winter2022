178178b397e03d7d17950a1184f3d75e4d768c90
mshelcole <97192890+mshelcole@users.noreply.github.com>
2022-02-28 18:09:03 -0800

    Update javascipt.js

--- artifact05/javascipt.js
+++ artifact05/javascipt.js
@@ -1,50 +1,51 @@
-var myImages =["images/Mount_Rainier_from_west.jpg", 
-               "images/Mount_Baker_from_Boulder_Creek.jpg",
-               "images/Mount_Olympus_Washington.jpg",
-               "images/400px-Mount_Adams_from_Cultus_Creek_Trail.jpg",
-               "images/MSH82_st_helens_plume_from_harrys_ridge_05-19-82.jpg"];
+var myImages = ["images/Mount_Rainier_from_west.jpg",
+    "images/Mount_Baker_from_Boulder_Creek.jpg",
+    "images/Mount_Olympus_Washington.jpg",
+    "images/400px-Mount_Adams_from_Cultus_Creek_Trail.jpg",
+    "images/MSH82_st_helens_plume_from_harrys_ridge_05-19-82.jpg"
+];
 
 
-var captionImages =["Mount Rainier","Mount Baker","Mount Olympus","Mount Adams","Mount St. Helens"];
+var captionImages = ["Mount Rainier", "Mount Baker", "Mount Olympus", "Mount Adams", "Mount St. Helens"];
 
- var index=0; 
+var index = 0;
 
- function updateImage(){
- document.getElementById("slideshow").src = myImages[index];
- document.getElementById("slideshow").alt= captionImages[index];
- document.getElementById("caption").textContent = captionImages[index]; 
-} 
+function updateImage() {
+    document.getElementById("slideshow").src = myImages[index];
+    document.getElementById("slideshow").alt = captionImages[index];
+    document.getElementById("caption").textContent = captionImages[index];
+}
 
-function next(){
- if (myImages.length == index+1)
- index=0;
- else
- index++;
- updateImage();
-} 
- 
-
-function back(){
- if (index===0)
- index=myImages.length-1;
- else
- index--;
- 
- updateImage();
-} 
-
-var nextButton = document.getElementById("next"); 
-var previousButton = document.getElementById("previous"); 
-
-previousButton.addEventListener("click",back,false);
-nextButton.addEventListener("click",next,false); 
-
-
-
-function autoSlide(){
-if (document.getElementById("auto").checked)
- next(); 
+function next() {
+    if (myImages.length == index + 1)
+        index = 0;
+    else
+        index++;
+    updateImage();
 }
 
 
-setInterval(autoSlide,2000); // Next
+function back() {
+    if (index === 0)
+        index = myImages.length - 1;
+    else
+        index--;
+
+    updateImage();
+}
+
+var nextButton = document.getElementById("next");
+var previousButton = document.getElementById("previous");
+
+previousButton.addEventListener("click", back, false);
+nextButton.addEventListener("click", next, false);
+
+
+
+function autoSlide() {
+    if (document.getElementById("auto").checked)
+        next();
+}
+
+
+setInterval(autoSlide, 2000); // Next
